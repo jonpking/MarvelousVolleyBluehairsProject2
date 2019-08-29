65acf3e4f7bb653bea252e2c0ed213f0c4fceef5
Enigma <jonpking@gmail.com>
2019-08-28 22:11:02 -0700

    set up basic api routes for get/post/delete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,45 +1,81 @@
 const db = require("../models");
 
-module.exports = function(app) {
-  app.get("/api/authors", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findAll({
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+module.exports = function (app) {
+  // get all games owned by a single user
+  app.get("/api/games", function (req, res) {
+    db.Games.findAll({
+      where: {
+        // user id = person who is logged in ?
+      },
+      include: [db.Users]
+    }).then(function (dbGames) {
+      res.json(dbGames);
     });
   });
 
-  app.get("/api/authors/:id", function(req, res) {
-    // Here we add an "include" property to our options in our findOne query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.Author.findOne({
+  // get all games on wishlist of a single user
+  app.get("/api/games/wishlist", function (req, res) {
+    db.Games.findAll({
+      where: {
+        wishlisted: true
+        // user id = person who is logged in ?
+      },
+      include: [db.Users]
+    }).then(function (dbGames) {
+      res.json(dbGames);
+    });
+  });
+
+  // get a single game owned by a single user (search functionality)
+  app.get("/api/games/:id", function (req, res) {
+    db.Games.findOne({
       where: {
         id: req.params.id
       },
-      include: [db.Post]
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+      include: [db.Users]
+    }).then(function (dbGames) {
+      res.json(dbGames);
+    });
+  });
+  // not sure if this will work properly or not
+
+  // app.get("/api/games/:page", function (req, res) {
+  //   if (req.params.page === wishlist) {
+  //     db.Games.findAll({
+  //       where: {
+  //         wishlisted: true
+  //         // user id = person who is logged in ?
+  //       },
+  //       include: [db.Users]
+  //     }).then(function (dbGames) {
+  //       res.json(dbGames);
+  //     });
+  //   }
+  //   if (req.params.page === home) {
+  //     db.Games.findAll({
+  //       where: {
+  //         // user id = person who is logged in ?
+  //       },
+  //       include: [db.Users]
+  //     }).then(function (dbGames) {
+  //       res.json(dbGames);
+  //     });
+  //   }
+  // });
+
+  app.post("/api/games", function (req, res) {
+    db.Games.create(req.body).then(function (dbGames) {
+      res.json(dbGames);
     });
   });
 
-  app.post("/api/authors", function(req, res) {
-    db.Author.create(req.body).then(function(dbAuthor) {
-      res.json(dbAuthor);
-    });
-  });
-
-  app.delete("/api/authors/:id", function(req, res) {
-    db.Author.destroy({
+  app.delete("/api/games/:id", function (req, res) {
+    db.Games.destroy({
       where: {
         id: req.params.id
       }
-    }).then(function(dbAuthor) {
-      res.json(dbAuthor);
+    }).then(function (dbGames) {
+      res.json(dbGames);
     });
   });
-
 };
