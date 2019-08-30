6cfb8220b805223370fab60150e819e07b30feeb
Elvia <evfranco@hush.ai>
2019-08-30 16:08:28 -0700

    Updates

---
+++ public/assets/js/api2Inventory.js
@@ -0,0 +1,18 @@
+$("#searchSubmit").on("click", function () {
+    $("#gameSearchModal").modal("show");
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function (response) {
+    //     // board game api stuff
+    // });
+});
+
+// $("#searchForm").on("submit", gamesToDB);
+
+// const gamesToDB = (gameInfo) => {
+//     $.post("/api/games", gameInfo, function () {
+
+//     });
+// };
---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function (app) {
+
+  app.get("/wishlist", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/wishlist.html"));
+  });
+
+  app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+
+  app.get("/inventory", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/inventory.html"));
+  });
+};
